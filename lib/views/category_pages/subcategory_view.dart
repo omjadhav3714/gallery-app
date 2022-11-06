cb09cf29ad9c205d72d6e3e9d327562bc5d73165
abby3010 <abhayubhale.30@gmail.com>
2022-11-06 17:46:32 +0530

    Data not found message added if no subcategories present

--- lib/views/category_pages/subcategory_view.dart
+++ lib/views/category_pages/subcategory_view.dart
@@ -39,46 +39,62 @@ class _SubCategoryViewState extends State<SubCategoryView> {
             );
           }
 
-          return DefaultTabController(
-            length: 10,
-            child: Scaffold(
-              bottomNavigationBar: const CustomBottomNavigationBar(selectedIndex: 1),
-              appBar: AppBar(
-                leading: const BackButtonWidget(),
-                elevation: 0,
-                title: Text(
-                  widget.category,
-                  style: const TextStyle(
-                    color: primaryColor,fontFamily: "Poppins", fontWeight: FontWeight.w600
+          try {
+            return DefaultTabController(
+              length: 10,
+              child: Scaffold(
+                bottomNavigationBar:
+                    const CustomBottomNavigationBar(selectedIndex: 1),
+                appBar: AppBar(
+                  leading: const BackButtonWidget(),
+                  elevation: 0,
+                  title: Text(
+                    widget.category,
+                    style: const TextStyle(
+                        color: primaryColor,
+                        fontFamily: "Poppins",
+                        fontWeight: FontWeight.w600),
                   ),
-                ),
-                backgroundColor: white,
-                bottom: TabBar(
-                  isScrollable: true,
-                  indicatorColor: primaryColor,
-                  indicatorWeight: 4,
-                  tabs: List.generate(
-                    snapshot.data!['sub_category'].length,
-                    (index) => Tab(
-                      child: Text(
-                        snapshot.data!['sub_category'][index]['text'],
-                        style: const TextStyle(color: black,fontFamily: "Poppins", fontWeight: FontWeight.w500),
+                  backgroundColor: white,
+                  bottom: TabBar(
+                    isScrollable: true,
+                    indicatorColor: primaryColor,
+                    indicatorWeight: 4,
+                    tabs: List.generate(
+                      snapshot.data!['sub_category'].length,
+                      (index) => Tab(
+                        child: Text(
+                          snapshot.data!['sub_category'][index]['text'],
+                          style: const TextStyle(
+                              color: black,
+                              fontFamily: "Poppins",
+                              fontWeight: FontWeight.w500),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              body: TabBarView(
-                children: List.generate(
-                  snapshot.data!['sub_category'].length,
-                  (index) => TemplateView(
-                    cat: widget.category,
-                    subCat: snapshot.data!['sub_category'][index]['text'],
+                body: TabBarView(
+                  children: List.generate(
+                    snapshot.data!['sub_category'].length,
+                    (index) => TemplateView(
+                      cat: widget.category,
+                      subCat: snapshot.data!['sub_category'][index]['text'],
+                    ),
                   ),
                 ),
               ),
-            ),
-          );
+            );
+          } catch (e) {
+            return const Scaffold(
+              body: Center(
+                child: Text(
+                  "Data not found!",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            );
+          }
         });
   }
 }
